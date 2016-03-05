b9f7883556311581c4d2dbc63d18aa0ad41889f1
Simon Apen-Sadler <sapensadler@gmail.com>
2016-03-05 20:53:12 +0000

    Compute the lcm of 1-20 using a fold (accumulate in C++)

---
+++ problem5/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+long long gcd(long long a, long long b) {
+    while(b != 0) {
+        int t = b;
+        b = a % b;
+        a = t;
+    }
+}
+
+long long lcm(long long a, long long b) {
+    return (a*b)/gcd(a, b);
+}
+
+int main() {
+    std::vector<long long> v;
+    v.reserve(20);
+    for(long long i = 1; i <= 20; i++) {
+        v.push_back(i);
+    }
+    std::cout << std::accumulate(v.begin(), v.end(), 1L, lcm) << std::endl;
+}
