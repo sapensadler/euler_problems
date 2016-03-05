37889f41ab1f50bbd7332a852eddfba2f017366a
Simon Apen-Sadler <sapensadler@gmail.com>
2016-03-05 12:14:00 +0000

    Completed Problem 1
    Calculate the sum of all numbers between 1 and 1000 that are
    multiples of either 3 or 5

---
+++ problem1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int calculateSum(const int n) {
+    return (n*(n+1))/2;
+}
+
+int calculateSumOfMultiples(const int multiple1, const int multiple2, const int max) {
+    int sum = 0;
+    sum += multiple1 * calculateSum(max / multiple1);
+    sum += multiple2 * calculateSum(max / multiple2);
+    sum -= multiple1 * multiple2 * calculateSum(max / (multiple2 * multiple1));
+    return sum;
+}
+
+int main() {
+    std::cout << calculateSumOfMultiples(3, 5, 1000);
+    return 0;
+}
+
