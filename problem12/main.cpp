a6e91c27eef267591263140a44b27e7890201453
Simon Apen-Sadler <sapensadler@gmail.com>
2016-04-13 19:38:20 +0100

    Solved Problem 12

---
+++ problem12/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cassert>
+
+long long getNumberOfDivisors(long long n) {
+    assert(n > 0);
+    
+    if (n < 2) {
+        return 1;
+    }
+    int divisors = 2;
+    for (int i = 1; i*i <= n; i++) {
+        if (n % i == 0) {
+            divisors += (i*i != n) ? 2 : 1;
+        }
+    }
+    return divisors;
+}
+long long getTriangleNumber(long long n) {
+    return (n*(n+1))/2;
+}
+
+int main() {
+    int i = 0;
+    while(getNumberOfDivisors(getTriangleNumber(++i)) < 500) ;
+    std::cout << getTriangleNumber(i) << std::endl;
+    
+}
+
