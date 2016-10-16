a37f0821ed41df0c06d782e0878f420d2702bb21
Simon Apen-Sadler <sapensadler@gmail.com>
2016-10-16 16:38:30 +0100

    Solved Problem 20

---
+++ problem20/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <gmpxx.h>
+
+mpz_class factorial(mpz_class n) {
+    mpz_class factorial = 1;
+    while(n >= 1) {
+        factorial *= n;
+        n = n - 1;
+    }
+    return factorial;
+}
+
+int main() {
+    mpz_class num = factorial(mpz_class(100));
+    mpz_class sum = 0;
+    const mpz_class base = 10;
+    while (num > 0) {
+        sum += num % base;
+        num /= base;
+    }
+    std::cout << sum.get_str() << std::endl;
+    return 0;
+}
+
