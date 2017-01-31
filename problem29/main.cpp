c8ea2df6ed4ab81a6c26f611d4014733c1ce55f3
Simon Apen-Sadler <sapensadler@gmail.com>
2017-01-31 19:36:48 +0000

    Solved problem 29 using brute false
    Removed unnecessary dependency from problem 29
    Fixed spaces and tabes in results file

---
+++ problem29/main.cpp
@@ -0,0 +1,26 @@
+#include <gmpxx.h>
+#include <iostream>
+#include <set>
+
+/**
+  * Find the number of unique integers of a^b for all a,b in [2,100] in Z
+  */
+
+mpz_class MIN = 2;
+mpz_class MAX = 100;
+
+int main() {
+
+    std::set<mpz_class> numbers;
+    for(mpz_class a = MIN; a <= MAX; a++) {
+            mpz_class a_exp_b = a;
+        for(mpz_class b=MIN; b <= MAX; b++) {
+            a_exp_b *= a;
+            numbers.insert(a_exp_b);
+        }
+    }
+    std::cout << numbers.size();
+
+    return 0;
+}
+
